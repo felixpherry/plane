3ade15374acdda67a4cce4e52948f73e24ec89af
Felix Pherry <71205609+felixpherry@users.noreply.github.com>
2026-03-17 13:25:31 +0700

    feat: wire custom fields section into issue detail sidebar

--- apps/web/core/components/issues/issue-detail/sidebar.tsx
+++ apps/web/core/components/issues/issue-detail/sidebar.tsx
@@ -46,6 +46,7 @@ import { SidebarPropertyListItem } from "@/components/common/layout/sidebar/prop
 import { IssueCycleSelect } from "./cycle-select";
 import { IssueLabel } from "./label";
 import { IssueModuleSelect } from "./module-select";
+import { CustomFieldsSection } from "./custom-fields";
 import type { TIssueOperations } from "./root";
 
 type Props = {
@@ -258,6 +259,14 @@ export const IssueDetailsSidebar = observer(function IssueDetailsSidebar(props: 
                 disabled={!isEditable}
               />
             </SidebarPropertyListItem>
+
+            {/* Custom Fields Section */}
+            <CustomFieldsSection
+              workspaceSlug={workspaceSlug}
+              projectId={projectId}
+              issueId={issueId}
+              disabled={!isEditable}
+            />
 
             <IssueWorklogProperty
               workspaceSlug={workspaceSlug}
