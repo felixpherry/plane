863a81249f63e764e35ff9f9177ecb75596f974a
Felix Pherry <71205609+felixpherry@users.noreply.github.com>
2026-03-30 15:42:09 +0700

    feat: register worklog types in index

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -53,6 +53,7 @@ export * from "./view-props";
 export * from "./views";
 export * from "./waitlist";
 export * from "./webhook";
+export * from "./worklog";
 export * from "./workspace";
 export * from "./workspace-draft-issues/base";
 export * from "./workspace-notifications";
